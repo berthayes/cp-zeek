eface5b13c08c0aad8f8ddf341a499596500df3b
Bert <berthayes@users.noreply.github.com>
2020-07-26 23:11:33 -0500

    fixed c3 vs connect
    bugfix

--- rc.sh
+++ rc.sh
@@ -5,7 +5,7 @@ key="/Users/bhayes/Downloads/bert_confluent_aws_keypem.pem"
 
 
 if [ $1 = "c" ]; then
-	for i in `cat connect_nodes.txt`; 
+	for i in `cat c3_nodes.txt`;
 	do /usr/bin/ssh -i $key $i $2 && echo && echo "remote_command_successful on $(/usr/bin/nslookup $i | grep name | awk {'print $4'}) I guess";
 	done
 
