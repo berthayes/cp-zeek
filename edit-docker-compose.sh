6dbd4c52035f2c8d44209a2cb0cdc77fbcac8cb2
Bert <berthayes@users.noreply.github.com>
2020-07-28 09:13:07 -0500

    Update edit-docker-compose.sh

--- edit-docker-compose.sh
+++ edit-docker-compose.sh
@@ -2,4 +2,4 @@
 # Dumb script to edit a template file and replace the ksqldb-server line
 
 DOCKER_COMPOSE_FILE="/home/ubuntu/cp-zeek/template-docker-compose.yml"
-/bin/sed -e 's/      CONTROL_CENTER_KSQL_KSQLDB1_ADVERTISED_URL: \"http:\/\/ksqldb-server:8088\"/      CONTROL_CENTER_KSQL_KSQLDB1_ADVERTISED_URL: \"http:\/\/'$1'.confluentpublicsector.net\"/' $DOCKER_COMPOSE_FILE >docker-compose.yml
+/bin/sed -e 's/      CONTROL_CENTER_KSQL_KSQLDB1_ADVERTISED_URL: \"http:\/\/ksqldb-server:8088\"/      CONTROL_CENTER_KSQL_KSQLDB1_ADVERTISED_URL: \"http:\/\/'$1'.confluentpublicsector.net:443\"/' $DOCKER_COMPOSE_FILE >/home/ubuntu/cp-zeek/docker-compose.yml
