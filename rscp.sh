36d5e7bab647b79c44d34d1ddfa349bd55312a00
Bert <berthayes@users.noreply.github.com>
2020-10-03 20:47:13 -0500

    specify ubuntu user

--- rscp.sh
+++ rscp.sh
@@ -6,17 +6,17 @@ key="/home/ubuntu/.ssh/id_rsa"
 
 if [ $1 = "c" ]; then
 	for i in `cat c3_nodes.txt`; 
-	do /usr/bin/scp -i $key $2 $i:/home/ubuntu/$2 && echo && echo "File $2 copied to $(/usr/bin/nslookup $i | grep name | awk {'print $4'}), I suppose";
-	done
+	do (/usr/bin/scp -i $key $2 ubuntu@$i:/home/ubuntu/$2 && echo && echo "File $2 copied to $(/usr/bin/nslookup $i | grep name | awk {'print $4'}), I suppose") & done
+	wait
 
 elif [ $1 = "a" ]; then
 	for i in `cat all_nodes.txt`;
-	do /usr/bin/scp -i $key $2 $i:/home/ubuntu/$2 && echo && echo "File $2 copied to $(/usr/bin/nslookup $i | grep name | awk {'print $4'}), I suppose";
-	done
+	do (/usr/bin/scp -i $key $2 ubuntu@$i:/home/ubuntu/$2 && echo && echo "File $2 copied to $(/usr/bin/nslookup $i | grep name | awk {'print $4'}), I suppose") & done
+	wait
 
 elif [ $1 = "k" ]; then
 	for i in `cat ksqldb_nodes.txt`;
-	do /usr/bin/scp -i $key $2 $i:/home/ubuntu/$2 && echo && echo "File $2 copied to $(/usr/bin/nslookup $i | grep name | awk {'print $4'}), I suppose";
-        done
+	do (/usr/bin/scp -i $key $2 ubuntu@$i:/home/ubuntu/$2 && echo && echo "File $2 copied to $(/usr/bin/nslookup $i | grep name | awk {'print $4'}), I suppose") & done
+        wait
 	
 fi
