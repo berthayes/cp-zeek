e1d30a8b776ecf26f1c3d1a828cc81c018a1562c
Bert <berthayes@users.noreply.github.com>
2020-10-03 20:25:53 -0500

    Update rc.sh
    specify ubuntu username

--- rc.sh
+++ rc.sh
@@ -6,14 +6,14 @@ key="/home/ubuntu/.ssh/id_rsa"
 
 if [ $1 = "c" ]; then
 	for i in `cat c3_nodes.txt`;
-	do /usr/bin/ssh -i $key $i $2 && echo && echo "remote_command_successful on $(/usr/bin/nslookup $i | grep name | awk {'print $4'}) I guess";
+	do /usr/bin/ssh -i $key ubuntu@$i $2 && echo && echo "remote_command_successful on $(/usr/bin/nslookup $i | grep name | awk {'print $4'}) I guess";
 	done
 
 
 
 elif [ $1 = "a" ]; then
 	for i in `cat all_nodes.txt`;
-	do /usr/bin/ssh -i $key $i $2 && echo && echo "remote_command_successful on $(/usr/bin/nslookup $i | grep name | awk {'print $4'}) I guess";
+	do /usr/bin/ssh -i $key ubuntu@$i $2 && echo && echo "remote_command_successful on $(/usr/bin/nslookup $i | grep name | awk {'print $4'}) I guess";
 	done
 
 elif [ $1 = "k" ]; then
