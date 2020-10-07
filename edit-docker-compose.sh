e102a638852a3c2cc06351b8e6015684362c6f79
Bert <berthayes@users.noreply.github.com>
2020-10-07 01:12:08 -0500

    Update edit-docker-compose.sh

--- edit-docker-compose.sh
+++ edit-docker-compose.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Dumb script to edit a template file and replace the ksqldb-server line
 
-WORKSHOP_DOMAIN=`cat /home/ubuntu/cp-zeek/yak_shaving.conf | grep workshop_domain | awk -F= {'print $2'} | tr -d '[:space:]'`
+WORKSHOP_DOMAIN=`cat yak_shaving.conf | grep workshop_domain | awk -F= {'print $2'} | tr -d '[:space:]'`
 # This assumes that the domain name that the workshop runs in is managed by AWS Route 53
 
 
@@ -9,7 +9,7 @@ DOCKER_COMPOSE_FILE="/home/ubuntu/cp-zeek/docker-compose.yml"
 
 
 # Run this if you want to (have to) run Confluent Control Center on port 80 and ksqldb server (REST API) on port 443 with no encryption
-/bin/sed -e 's/      CONTROL_CENTER_KSQL_KSQLDB1_ADVERTISED_URL: \"http:\/\/localhost:8088\"/      CONTROL_CENTER_KSQL_KSQLDB1_ADVERTISED_URL: \"http:\/\/'$1'.'$WORKSHOP_DOMAIN':443\"/' -e 's/      - "9021:9021"/      - "80:9021"/' -e 's/      - "8088:8088"/      - "443:8088"/' $DOCKER_COMPOSE_FILE > /home/ubuntu/cp-zeek/workshop-docker-compose.yml
+/bin/sed -e 's/      CONTROL_CENTER_KSQL_KSQLDB1_ADVERTISED_URL: \"http:\/\/localhost:8088\"/      CONTROL_CENTER_KSQL_KSQLDB1_ADVERTISED_URL: \"http:\/\/'$1'.'$WORKSHOP_DOMAIN':443\"/' -e 's/      - "9021:9021"/      -"80:9021"/' -e 's/      - "8088:8088"/      -"443:8088"/' $DOCKER_COMPOSE_FILE > /home/ubuntu/cp-zeek/workshop-docker-compose.yml
 
 
 # Run this if you are sane and can run these services on their default ports (9021 for Confluent Control Center and 8088 for ksqlDB)
