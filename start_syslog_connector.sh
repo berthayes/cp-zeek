85b7d0c648aef8d8531bcdc7e644ba6fb1b02c21
Bert <berthayes@users.noreply.github.com>
2020-10-14 22:41:30 -0500

    auto-start syslog connector

---
+++ start_syslog_connector.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+curl localhost:8083/connectors -X POST -H "Content-Type: application/json" -d '{
+	"name": "syslog",
+	"config": {
+		"tasks.max": "1",
+		"connector.class": "io.confluent.connect.syslog.SyslogSourceConnector",
+		"syslog.port": "5140",
+		"syslog.listener": "UDP",
+		"confluent.topic.bootstrap.servers": "broker:29092",
+		"confluent.topic.replication.factor": "1" 
+	}
+}'
