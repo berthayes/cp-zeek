2eff4ce453f074b31f32cc88e80529d0f34bbea5
Bert <berthayes@users.noreply.github.com>
2020-10-16 23:19:43 -0500

    adding ad_hosts

--- start_urlhaus_spooldir.sh
+++ start_urlhaus_spooldir.sh
@@ -6,9 +6,9 @@ curl localhost:8083/connectors -X POST -H "Content-Type: application/json" -d '{
 		"connector.class": "com.github.jcustenborder.kafka.connect.spooldir.SpoolDirCsvSourceConnector",
 		"tasks.max": "1",
 		"topic": "urlhaus",
-		"input.path": "/var/spooldir/csv_input",
-		"finished.path": "/var/spooldir/csv_finished",
-		"error.path": "/var/spooldir/csv_errors",
+		"input.path": "/var/spooldir/urlhaus/csv_input",
+		"finished.path": "/var/spooldir/urlhaus/csv_finished",
+		"error.path": "/var/spooldir/urlhaus/csv_errors",
 	  "input.file.pattern": ".*\\.csv$",
     "schema.generation.enabled": true,
     "csv.first.row.as.header": true
