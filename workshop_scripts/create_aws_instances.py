494234ca3138f348eaac60605ad25cc2c9238deb
Bert <berthayes@users.noreply.github.com>
2020-12-01 22:50:31 -0600

    Updated for Ansible

--- workshop_scripts/create_aws_instances.py
+++ workshop_scripts/create_aws_instances.py
@@ -1,14 +1,41 @@
 import boto3
 from configparser import ConfigParser
+import argparse
+import sys
 
 # create a bunch of  new EC2 instance
-# TODO: create command line args to specify config file at least
+parser = argparse.ArgumentParser(description=
+    '''This script creates a bunch of EC2 hosts in AWS for running Docker & Demos''')
+parser.add_argument('-f', dest='config_file', action='store', help='the full path of the config file')
+parser.add_argument('-n', dest='how_many_hosts_to_create', action='store', help='how many EC2 instances to create', type=int)
+parser.add_argument('-m', dest='hosts_already_created', action='store', help='How many instances already created - default is 0', default=0, type=int)
+
+#how_many_hosts_to_create = 3
+#hosts_already_created = 0
+
+args = parser.parse_args()
+
+if args.config_file:
+    config_file = args.config_file
+else:
+    config_file = 'yak_shaving.conf'
+
+
+if args.how_many_hosts_to_create:
+    how_many_hosts_to_create = args.how_many_hosts_to_create
+else:
+    print("Need to specify how many hosts to create with -n")
+    sys.exit()
+
+if args.hosts_already_created:
+    hosts_already_created = args.hosts_already_created
+else:
+    hosts_already_created = 0
+
 
 cfg = ConfigParser()
-cfg.read('yak_shaving.conf')
+cfg.read(config_file)
 
-how_many_hosts_to_create = 3
-hosts_already_created = 0
 
 snapshot_id = cfg.get('aws', 'snapshot_id')
 security_group_id = cfg.get('aws', 'security_group_id')
--- workshop_scripts/edit-docker-compose.sh
+++ workshop_scripts/edit-docker-compose.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 # Dumb script to edit a template file and replace the ksqldb-server line
 
-conf_file="/home/ubuntu/cp-zeek/workshop_scripts/yak_shaving.conf"
-
-WORKSHOP_DOMAIN=`cat $conf_file | grep workshop_domain | awk -F= {'print $2'} | tr -d '[:space:]'`
-# This assumes that the domain name that the workshop runs in is managed by AWS Route 53
+PUBLIC_HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/public-hostname)
 
 
 DOCKER_COMPOSE_FILE="/home/ubuntu/cp-zeek/docker-compose.yml"
 
 
 # Run this if you want to (have to) run Confluent Control Center on port 80 and ksqldb server (REST API) on port 443 with no encryption
-/bin/sed -e 's/      CONTROL_CENTER_KSQL_KSQLDB1_ADVERTISED_URL: \"http:\/\/localhost:8088\"/      CONTROL_CENTER_KSQL_KSQLDB1_ADVERTISED_URL: \"http:\/\/'$1'.'$WORKSHOP_DOMAIN':443\"/' -e 's/      - "9021:9021"/      - "80:9021"/' -e 's/      - "8088:8088"/      - "443:8088"/' $DOCKER_COMPOSE_FILE > /home/ubuntu/cp-zeek/workshop-docker-compose.yml
+/bin/sed -e 's/      CONTROL_CENTER_KSQL_KSQLDB1_ADVERTISED_URL: \"http:\/\/localhost:8088\"/      CONTROL_CENTER_KSQL_KSQLDB1_ADVERTISED_URL: \"http:\/\/'$PUBLIC_HOSTNAME':443\"/' -e 's/      - "9021:9021"/      - "80:9021"/' -e 's/      - "8088:8088"/      - "443:8088"/' $DOCKER_COMPOSE_FILE > /home/ubuntu/cp-zeek/workshop-docker-compose.yml
 
 
 # Run this if you are sane and can run these services on their default ports (9021 for Confluent Control Center and 8088 for ksqlDB)
-#/bin/sed -e 's/      CONTROL_CENTER_KSQL_KSQLDB1_ADVERTISED_URL: \"http:\/\/ksqldb-server:8088\"/      CONTROL_CENTER_KSQL_KSQLDB1_ADVERTISED_URL: \"http:\/\/'$1'.'$WORKSHOP_DOMAIN':8088\"/' $DOCKER_COMPOSE_FILE > /home/ubuntu/cp-zeek/workshop-docker-compose.yml
+#/bin/sed -e 's/      CONTROL_CENTER_KSQL_KSQLDB1_ADVERTISED_URL: \"http:\/\/ksqldb-server:8088\"/      CONTROL_CENTER_KSQL_KSQLDB1_ADVERTISED_URL: \"http:\/\/'$PUBLIC_HOSTNAME':8088\"/' $DOCKER_COMPOSE_FILE > /home/ubuntu/cp-zeek/workshop-docker-compose.yml
